52179557df2d62aca64ecbd52bfd4f2a3b066d95
Tyler Fritz <tfritzy@gmail.com>
2020-07-03 16:54:53 -0700

    Added Particle system pack and improved fire bolt particles

---
+++ Assets/Scripts/Character/Cows/Dummy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Dummy : Cow
+{
+    public override CowType CowType => CowType.WimpyCow;
+    public Allegiance EnemyOverride;
+    public Allegiance AllegianceOverride;
+
+    public override void Initialize()
+    {
+        this.Name = "Dummy " + Guid.NewGuid().ToString("N").Substring(0, 8);
+        this.DropTable = new WimpyCowDropTable();
+        base.Initialize();
+        this.Enemies = new System.Collections.Generic.HashSet<Allegiance> {EnemyOverride}; 
+        this.Allegiance = AllegianceOverride;
+    }
+
+    protected override void SetInitialStats()
+    {
+        this.Health = 100000;
+        this.MaxHealth = Health;
+        this.Damage = 2;
+        this.AttackSpeed = 1;
+        this.TargetFindRadius = 10;
+        this.AttackRange = 10f;
+        this.MovementSpeed = 0f;
+        this.PrimarySkill = new FireBolt();
+    }
+
+    public override void PrimaryAttack()
+    {
+        base.PrimaryAttack();
+        float lastLastAttackTime = this.PrimarySkill.LastAttackTime;
+        this.PrimarySkill = new FireBolt();
+        this.PrimarySkill.LastAttackTime = lastLastAttackTime;
+    }
+}
--- Assets/Scripts/Character/Skills/Projectile.cs
+++ Assets/Scripts/Character/Skills/Projectile.cs
@@ -6,7 +6,8 @@ public class Projectile : MonoBehaviour
     public delegate void DamageEnemy(Character attacker, Character target);
     private DamageEnemy damageEnemyHandler;
     private Character attacker;
-    
+    private const string trailGroupName = "Trail";
+
     public void Initialize(DamageEnemy damageEnemyHandler, Character attacker)
     {
         this.damageEnemyHandler = damageEnemyHandler;
@@ -24,7 +25,15 @@ public class Projectile : MonoBehaviour
         if (attacker.Enemies.Contains(otherCharacter.Allegiance))
         {
             damageEnemyHandler(attacker, otherCharacter);
+            DetachParticles(transform.Find(trailGroupName));
             Destroy(this.gameObject);
         }
+    }
+
+    private void DetachParticles(Transform particleGroup)
+    {
+        particleGroup.parent = null;
+        particleGroup.GetComponent<ParticleSystem>().Stop();
+        Destroy(particleGroup.gameObject, 1f);
     }
 }
