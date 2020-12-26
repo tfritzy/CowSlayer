b1d1e1b430f9152494d174931fee78f4a009c11a
Tyler Fritz <tfritzy@gmail.com>
2020-12-25 16:35:42 -0800

    Adding fire source skill map

---
+++ Assets/Scripts/Character/Player/FireSorcress.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSorcress : Player
+{
+    public enum SkillType {
+        Firebolt,
+        Fireball,
+        Meteor
+    }
+
+    private Dictionary<SkillType, Skill> _skills;
+    public Dictionary<SkillType, Skill> Skills
+    {
+        get {
+            if (_skills == null)
+            {
+                _skills = new Dictionary<SkillType, Skill>()
+                {
+                    {
+                        SkillType.Firebolt,
+                        new FireBolt(),
+                    }
+                }
+            }
+
+            return _skills;
+        }
+    }
+}
