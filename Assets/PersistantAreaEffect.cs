4a59fe38f7ed781d65460f83f1c580ebb8fa572a
Tyler Fritz <tfritzy@gmail.com>
2021-01-10 11:19:55 -0800

    Added decal to ground fire

--- Assets/PersistantAreaEffect.cs
+++ Assets/PersistantAreaEffect.cs
@@ -7,6 +7,7 @@ public abstract class PersistantAreaEffect : MonoBehaviour
     public float TimeBeforeStart;
     public bool Enabled = false;
     public float Duration;
+    public GameObject Decal;
     protected abstract float MinTimeBetweenEffectApplications { get; }
     protected Dictionary<GameObject, float> recentHits;
     protected abstract void ApplyEffect(GameObject gameObject);
@@ -23,6 +24,7 @@ public abstract class PersistantAreaEffect : MonoBehaviour
     void Start()
     {
         birthTime = Time.time;
+        this.Decal = this.transform.Find("Decal")?.gameObject;
     }
 
     private void OnTriggerStay(Collider other)
@@ -51,6 +53,23 @@ public abstract class PersistantAreaEffect : MonoBehaviour
     {
         Enabled = true;
         recentHits = new Dictionary<GameObject, float>();
+        PositionDecal(Decal);
         Destroy(this.gameObject, Duration + .1f);
+    }
+
+    protected void PositionDecal(GameObject decal)
+    {
+        if (decal == null)
+        {
+            return;
+        }
+
+        Ray ray = new Ray(this.transform.position, Vector3.down);
+        Physics.Raycast(ray, out RaycastHit hitInfo);
+        decal.transform.position = hitInfo.point;
+        decal.transform.forward = hitInfo.normal * -1f;
+        decal.transform.rotation = decal.transform.rotation * Quaternion.AngleAxis(Random.Range(0, 360), decal.transform.up);
+        decal.transform.parent = null;
+        Destroy(decal, 5f);
     }
 }
